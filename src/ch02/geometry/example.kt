0552ab3c4881031542d6052233e0c8a76a583e5c
waldo(이준성) <zippo91@coupang.com>
2018-07-03 23:52:24 +0900

    [waldo-000] 코틀린 2장

--- src/ch02/geometry/example.kt
+++ src/ch02/geometry/example.kt
@@ -2,6 +2,7 @@ package ch02.geometry
 
 import ch02.color.enum.ColorType
 import ch02.geometry.shapes.createRandomRectangle
+import java.util.*
 
 /**
  * @author waldo:Athens , zippo91@coupang.com
@@ -23,6 +24,27 @@ fun main(args: Array<String>) {
     for(i in 50 downTo 1 step 2) {
         print(fizzBuzz(i))
     }
+
+    val binaryMaps = TreeMap<Char, String>()
+
+    for(c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryMaps.put(c, binary) //binaryMaps[c] = binary
+
+    }
+
+    for((letter, binary) in binaryMaps) {
+        println("$letter = $binary")
+    }
+
+    val list = arrayListOf("10", "11", "1001")
+    for((index, element) in list.withIndex()) {
+        println("$index = $element")
+    }
+
+    println(isLetter('b'))
+    println(isNotDigit('b'))
+    println(recognize('b'))
 
 }
 
@@ -58,4 +80,13 @@ fun fizzBuzz(i: Int) = when {
     i % 3 == 0 -> "Fizz "
     i % 5 == 0 -> "Buzz "
     else -> "$i "
+}
+fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+
+fun isNotDigit(c: Char) = c !in '0'..'9'
+
+fun recognize(c: Char) = when(c) {
+    in 'a'..'z', in 'A'..'Z' -> "It's a letter"
+    in '0'..'9' -> "It's a digit"
+    else -> "I don't know"
 }
