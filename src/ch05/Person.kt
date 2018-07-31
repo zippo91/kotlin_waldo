9de44c765552c51027ad6483cedf3cc4e9c02267
waldo(이준성) <zippo91@coupang.com>
2018-08-01 00:22:14 +0900

    3장

---
+++ src/ch05/Person.kt
@@ -0,0 +1,6 @@
+package ch05
+
+data class Person (
+    val name: String,
+    val age: Int
+)
---
+++ src/ch05/ch05.kt
@@ -0,0 +1,51 @@
+package ch05
+
+fun salute() = println("Salute!") // 최상위함수
+
+fun main(args: Array<String>) {
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+
+    println(people.maxBy { it.age }) // maxBy kotlin에서 제공하는 collection
+    //it는 자동 생성된 파라미터 이름
+
+//    println(people.maxBy(Person::age))  //멤버 참조
+//    println(people.maxBy { person: Person-> person.age })
+//    println(people.maxBy { person: Person-> person.age})
+    //컴파일러가 타입추론 Person 생략가능
+    println(people.maxBy { person-> person.age})
+
+    val getAge = {person : Person -> person.age} //람다를 변수에 저장할 때는 타입을 넣어줘야함
+
+    //val names = people.joinToString(separator = " ", transform = {person: Person -> person.name })
+    val names = people.joinToString(" ") {person: Person -> person.name }
+    println(names)
+
+    val error = listOf("403 Forbidden", "404 Not Found", "500 Internal Server Error")
+
+    printMessagesWithPrefix(error, "Error")
+    printProblemCounts(error)
+
+    run(::salute) //최상위 함수도 멤버 참조로 참조가능
+}
+
+fun printMessagesWithPrefix(message: Collection<String>, prefix: String) {
+    message.forEach {
+        println("$prefix $it")
+    }
+}
+
+fun printProblemCounts(message: Collection<String>) {
+    var clientErrors = 0
+    var serverErrors = 0
+    //함수안의 변수는 함수가 끝나면 반환
+
+    message.forEach {
+        if(it.startsWith("4")) {
+            clientErrors++
+        } else if(it.startsWith("5")) {
+            serverErrors++
+        }
+    }
+
+    println("$clientErrors client errors, $serverErrors server errors")
+}
